ff136820ea80565082d8bd1ff34a062393200000
Noelia Rodriguez <noeliardp@gmail.com>
2022-10-27 19:28:18 +0200

    Add parent script for Ecoli RNAseq
    This script uses a for loop that will call a sample_analysing script for each sample

---
+++ scripts/run_pipeline.sh
@@ -0,0 +1,18 @@
+WD=~/testsim
+
+mkdir -p $WD/res/genome
+
+# Download the E.coli genome
+wget -O $WD/res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
+
+#Unzip E.coli genome 
+gunzip $WD/res/genome/ecoli.fasta.gz
+
+for sid in $(ls $WD/data/*.fastq.gz | xargs basename -a | cut -d_ -f1 | sort -u)
+do
+	 echo bash $WD/scripts/rna_pipeline.sh $sid 
+done
+
+mkdir -p $WD/out/multiqc
+
+multiqc -o $WD/out/multiqc $WD
