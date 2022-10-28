9d68ff74a1aeaa1eb562ef9fa14fd14188441800
Noelia Rodriguez <noeliardp@gmail.com>
2022-10-28 14:36:31 +0200

    Add absolute paths

--- scripts/analyse_sample.sh
+++ scripts/analyse_sample.sh
@@ -1,3 +1,5 @@
+wd=~/testsim #Define working directory
+
 if [ "$#" -ne 1 ]
 then
     echo "Usage: $0 <sampleid>"
@@ -6,29 +8,30 @@ fi
 
 sampleid=$1
 
+echo "Analysing sample ${sampleid}..."
 echo "Running FastQC..."
-mkdir -p out/fastqc
-fastqc -o out/fastqc data/${sampleid}_?.fastq.gz
+mkdir -p $wd/out/fastqc
+fastqc -o $wd/out/fastqc $wd/data/${sampleid}_?.fastq.gz
 echo
 
 echo "Running cutadapt..."
-mkdir -p log/cutadapt
-mkdir -p out/cutadapt
+mkdir -p $wd/log/cutadapt
+mkdir -p $wd/out/cutadapt
 cutadapt \
     -m 20 \
     -a AGATCGGAAGAGCACACGTCTGAACTCCAGTCA \
     -A AGATCGGAAGAGCGTCGTGTAGGGAAAGAGTGT \
-    -o out/cutadapt/${sampleid}_1.trimmed.fastq.gz \
-    -p out/cutadapt/${sampleid}_2.trimmed.fastq.gz data/${sampleid}_1.fastq.gz data/${sampleid}_2.fastq.gz \
-    > log/cutadapt/${sampleid}.log
+    -o $wd/out/cutadapt/${sampleid}_1.trimmed.fastq.gz \
+    -p $wd/out/cutadapt/${sampleid}_2.trimmed.fastq.gz $wd/data/${sampleid}_1.fastq.gz $wd/data/${sampleid}_2.fastq.gz \
+    > $wd/log/cutadapt/${sampleid}.log
 echo
 
 echo "Running STAR alignment..."
-mkdir -p out/star/${sampleid}
+mkdir -p $wd/out/star/${sampleid}
 STAR \
     --runThreadN 4 \
-    --genomeDir res/genome/star_index/ \
-    --readFilesIn out/cutadapt/${sampleid}_1.trimmed.fastq.gz out/cutadapt/${sampleid}_2.trimmed.fastq.gz \
+    --genomeDir $wd/res/genome/star_index/ \
+    --readFilesIn $wd/out/cutadapt/${sampleid}_1.trimmed.fastq.gz $wd/out/cutadapt/${sampleid}_2.trimmed.fastq.gz \
     --readFilesCommand zcat \
-    --outFileNamePrefix out/star/${sampleid}/
+    --outFileNamePrefix $wd/out/star/${sampleid}/
 echo
