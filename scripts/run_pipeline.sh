68a756cdeb2c3c4ab6dc235455bbb6e60467d06e
Noelia Rodriguez <noeliardp@gmail.com>
2022-10-28 14:29:35 +0200

    Add some comand comments

--- scripts/run_pipeline.sh
+++ scripts/run_pipeline.sh
@@ -4,7 +4,6 @@ wd=~/testsim
 echo "Downloading genome..."
 mkdir -p $wd/res/genome
 wget -c -O $wd/res/genome/ecoli.fasta.gz ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCF/000/005/845/GCF_000005845.2_ASM584v2/GCF_000005845.2_ASM584v2_genomic.fna.gz
-#Check if downloading was succesfull
 if [ "$?" -ne 0 ] # Checks if previous exit code is not equal to 0
 then
     echo "Error downloading file. Usage: bash scripts/$0"
@@ -13,8 +12,7 @@ fi
 echo 
 
 echo "Uncompressing genome..."
-#Unzip E.coli genome 
-gunzip -f $wd/res/genome/ecoli.fasta.gz
+gunzip -f $wd/res/genome/ecoli.fasta.gz #Force to overwrite existing file, it avoids having to answer "yes" when rerunning pipeline
 echo 
 
 echo "Running STAR index..."
@@ -30,9 +28,13 @@ echo
 for sid in $(ls $wd/data/*.fastq.gz | xargs basename -a | cut -d_ -f1 | sort -u)
 do
 	bash $wd/scripts/analyse_sample.sh $sid 
+	echo "Done"
+	echo
 done
 
+echo "Running MultiQC..."
 mkdir -p $wd/out/multiqc
 multiqc -o $wd/out/multiqc $wd
 echo
+
 echo "-------- Pipeline finished at $(date +'%d %h %y, %r')... --------"
